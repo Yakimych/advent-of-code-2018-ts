2329a2e9fd8c57558a5ffdb6beaa2e1cc0624f4b
Kyrylo Yakymenko <yakimych@gmail.com>
2018-12-15 15:34:41 +0100

    Visualize the current points

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Layer, Rect, Stage } from "react-konva";
 import "./App.css";
 import { Point } from "./inputData";
 import { applyVelocity } from "./pointLogic";
@@ -10,6 +11,7 @@ type Props = {
 type State = {
   currentIteration: number;
   step: number;
+  zoom: number;
   currentPoints: ReadonlyArray<Point>;
 };
 
@@ -17,10 +19,12 @@ class App extends React.Component<Props, State> {
   public state: Readonly<State> = {
     currentIteration: 0,
     step: 1,
+    zoom: 1,
     currentPoints: this.props.initialPoints,
   };
 
   public render() {
+    const { zoom } = this.state;
     return (
       <div className="App">
         <span>Current iteration: {this.state.currentIteration}</span>
@@ -33,12 +37,37 @@ class App extends React.Component<Props, State> {
           />
           <button onClick={this.handleNextClick}>Next</button>
         </div>
+        <span>Zoom level: {this.state.zoom}</span>
+        <div>
+          <button onClick={this.handleDecreaseZoomClick}>-</button>
+          <span>{this.state.zoom}</span>
+          <button onClick={this.handleIncreaseZoomClick}>+</button>
+        </div>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            {this.state.currentPoints.map((p) => (
+              <Rect
+                x={p.x * zoom}
+                y={p.y * zoom}
+                width={zoom}
+                height={zoom}
+                fill="green"
+              />
+            ))}
+          </Layer>
+        </Stage>
       </div>
     );
   }
 
   private handleStepChange = (e: any) =>
     this.setState({ step: Number(e.target.value) })
+
+  private handleIncreaseZoomClick = () =>
+    this.setState((prevState) => ({ zoom: prevState.zoom + 1 }))
+
+  private handleDecreaseZoomClick = () =>
+    this.setState((prevState) => ({ zoom: prevState.zoom - 1 }))
 
   private handlePrevClick = () => this.updatePoints(-this.state.step);
   private handleNextClick = () => this.updatePoints(this.state.step);
