85ad7becd12047e80aca917997e3b2e23d3a733a
Kyrylo Yakymenko <yakimych@gmail.com>
2018-12-16 12:18:36 +0100

    Make unzooming possible, align image to left/top point

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,12 @@ class App extends React.Component<Props, State> {
 
   public render() {
     const { zoom } = this.state;
-    const currentPoints = this.props.initialPoints.map((p) =>
+    const currentPoints = this.props.initialPoints.map(p =>
       applyVelocity(p, this.state.currentIteration),
     );
+
+    const leftMostX = Math.min(...currentPoints.map(p => p.x));
+    const topMostY = Math.min(...currentPoints.map(p => p.y));
 
     return (
       <div className="App">
@@ -42,15 +45,14 @@ class App extends React.Component<Props, State> {
         <span>Zoom level: {this.state.zoom}</span>
         <div>
           <button onClick={this.handleDecreaseZoomClick}>-</button>
-          <input value={this.state.zoom} onChange={this.handleZoomChange} />
           <button onClick={this.handleIncreaseZoomClick}>+</button>
         </div>
         <Stage width={window.innerWidth} height={window.innerHeight}>
           <Layer>
-            {currentPoints.map((p) => (
+            {currentPoints.map(p => (
               <Rect
-                x={p.x * zoom}
-                y={p.y * zoom}
+                x={(p.x - leftMostX) * zoom}
+                y={(p.y - topMostY) * zoom}
                 width={Math.max(zoom, 1)}
                 height={Math.max(zoom, 1)}
                 fill="green"
@@ -65,14 +67,27 @@ class App extends React.Component<Props, State> {
   private handleStepChange = (e: any) =>
     this.setState({ step: Number(e.target.value) })
 
-  private handleZoomChange = (e: any) =>
-    this.setState({ zoom: Number(e.target.value) })
+  private handleIncreaseZoomClick = () => {
+    this.setState(prevState => {
+      if (prevState.zoom >= 1) {
+        return { zoom: prevState.zoom + 1 };
+      } else if (prevState.zoom >= 0.5) {
+        return { zoom: 1 };
+      } else {
+        return { zoom: prevState.zoom * 2 };
+      }
+    });
+  }
 
-  private handleIncreaseZoomClick = () =>
-    this.setState((prevState) => ({ zoom: prevState.zoom + 1 }))
-
-  private handleDecreaseZoomClick = () =>
-    this.setState((prevState) => ({ zoom: prevState.zoom - 1 }))
+  private handleDecreaseZoomClick = () => {
+    this.setState(prevState => {
+      if (prevState.zoom >= 2) {
+        return { zoom: prevState.zoom - 1 };
+      } else {
+        return { zoom: prevState.zoom / 2 };
+      }
+    });
+  }
 
   private handlePrevClick = () =>
     this.setState({
